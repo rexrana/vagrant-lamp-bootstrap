8b97e1c0512228970b210efb3ded1543a1838415
Peter Hebert <peter@rexrana.ca>
2016-06-14 12:12:16 -0700

    removed decoupled provisioning files (now all inline bootstrap.sh)

--- home/wordpress.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# get the latest WP-CLI
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
-# make the file executable and move it to somewhere in your PATH
-chmod +x wp-cli.phar
-sudo mv wp-cli.phar /usr/local/bin/wp
